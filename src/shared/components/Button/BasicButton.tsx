c2be6e17a8913b03cade1e9ac757c7c4f57280a5
ramong26 <rlatndus21049@gmail.com>
2025-10-18 21:52:58 +0900

    feat/button - esling 수정 및 코드리뷰 반영

--- src/shared/components/Button/BasicButton.tsx
+++ src/shared/components/Button/BasicButton.tsx
@@ -38,35 +38,3 @@ export default function BasicButton({
     </button>
   );
 }
-
-// "use client";
-// import "./BasicButton.scss";
-
-// type ButtonType = "button" | "submit" | "reset" | undefined;
-// type ButtonVariant = "line" | "fill";
-
-// interface BasicButtonProps {
-//   children: React.ReactNode;
-//   onClick?: () => void;
-//   type?: ButtonType;
-//   variant?: ButtonVariant;
-//   className?: string;
-// }
-
-// // 조금 더 쉽게 할 수 있을 것 같다. if문 말고 다른 것은 없을까
-// export default function BasicButton({
-//   children,
-//   onClick,
-//   type = "button",
-//   variant = "fill",
-//   className,
-// }: BasicButtonProps) {
-//   const variantClass = `basic__button--${variant}`;
-//   const mergedClassName = ` ${variantClass} ${className}`.trim();
-
-//   return (
-//     <button className={mergedClassName} onClick={onClick} type={type}>
-//       {children}
-//     </button>
-//   );
-// }
