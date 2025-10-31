1e16e376fe26ca11b88ad7df3fe38a48ae66ce25
ramong26 <rlatndus21049@gmail.com>
2025-10-31 23:13:09 +0900

    feat/api-service - 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -50,6 +50,17 @@ export default [
       "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
       "no-undef": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+          vars: "all",
+          args: "after-used",
+        },
+      ],
     },
     settings: {
       react: {
