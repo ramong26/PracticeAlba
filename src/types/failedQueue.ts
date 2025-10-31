e0484c4e1bfb7ec54d42a1d7b0fd05403f5ea10b
ramong26 <rlatndus21049@gmail.com>
2025-10-31 23:02:34 +0900

    feat/api-service - Api 파일 수정

--- src/types/failedQueue.ts
+++ src/types/failedQueue.ts
@@ -1,8 +1,8 @@
 export type FailedQueueItem = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  resolve: (_token?: string | null) => void;
+  resolve: (token?: string | null) => void;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  reject: (_err?: any) => void;
+  reject: (err?: any) => void;
 };
 
 // 이거 아닌데.. 이러면 타입스크립트 쓰는 이유가 없는데...
